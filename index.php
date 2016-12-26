9b1a5a353266da0cedcf54aed78e9fea5c6d6417
Enzo Zapata <enzo0697@gmail.com>
2016-12-26 15:59:32 -0300

    imagen.php, form de index.php y lato.ttf

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Preparar video para Youtube</title>
+<meta charset="utf-8">
+</head>
+<body>
+<form action="imagen.php" method="post" enctype="multipart/form-data">
+<label for="url_imagen">URL de la imagén:</label><input type="text" name="url_imagen">
+<label for="texto">Texto:</label><input type="text" name="texto">
+<input type="submit" value="Subir">
+</form>
+</body>
+</html>
