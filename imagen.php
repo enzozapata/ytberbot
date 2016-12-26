e800d8f138397237f30f4b8d7cae2abea02c59ea
Enzo Zapata <enzo0697@gmail.com>
2016-12-26 18:52:40 -0300

    v0.9

--- imagen.php
+++ imagen.php
@@ -11,23 +11,21 @@
   $white = imagecolorallocate($jpg_image, $c1, $c2, $c3);
 
    //Set Path to Font File
-  $font_path = './arial.ttf';
+  $font_path = './arial-black.ttf';
 
   // Set Text to Be Printed On Image
   $text = $_POST['texto'];
 
   // Print Text On Image
-  imagettftext($jpg_image, 32, 0, 75, 75, $white, $font_path, $text);
-  imagettftext($jpg_image, 32, 0, 700, 500, $white, $font_path, 'www.AnimeXMega.net');
+  imagettftext($jpg_image, 44, 0, 75, 75, $white, $font_path, $text);
+  imagettftext($jpg_image, 24, 0, 700, 500, $white, $font_path, 'www.AnimeXMega.net');
 
   // Send Image to Browser
-  imagejpeg($jpg_image, 'img/imagen.jpg');
+  imagejpeg($jpg_image, 'img/'.$nombre_imagen.'.jpg');
 
   // Clear Memory
   imagedestroy($jpg_image);
-  if(exec('ffmpeg -i audio/audio.mp3 -f image2 -loop 1 -r 2 -i img/imagen.jpg -shortest -c:a copy -c:v libx264 -crf 23 -preset veryfast video/video.mp4')){
-	  
-  } else {
-	  
-  }
+  $random=mt_rand(1, 9999);
+  $ffmpeg_command='ffmpeg -i audio/audio.mp3 -f image2 -loop 1 -r 2 -i img/'.$random.'.jpg -shortest -c:a copy -c:v libx264 -crf 23 -preset veryfast video/'.$random.'.mp4';
+  exec($ffmpeg_command);
 ?>
