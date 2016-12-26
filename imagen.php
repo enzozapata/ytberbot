4b57ffe1630f981a2e339267cbcb846cf8cd7e3a
Enzo Zapata <enzo0697@gmail.com>
2016-12-26 17:24:05 -0300

    v0.9

--- imagen.php
+++ imagen.php
@@ -1,30 +1,31 @@
 <?php
   //Set the Content Type
-  header('Content-type: image/jpeg');
+ // header('Content-type: image/jpeg');
 
   // Create Image From Existing File
-  $jpg_image = imagecreatefromjpeg($_POST['url_imagen']);
- $c1 = mt_rand(50,200); //r(ed)
-     $c2 = mt_rand(50,200); //g(reen)
-     $c3 = mt_rand(50,200); //b(lue)
+ // $jpg_image = imagecreatefromjpeg($_POST['url_imagen']);
+ //$c1 = mt_rand(50,200); //r(ed)
+  //   $c2 = mt_rand(50,200); //g(reen)
+  //   $c3 = mt_rand(50,200); //b(lue)
   // Allocate A Color For The Text
-  $white = imagecolorallocate($jpg_image, $c1, $c2, $c3);
+  //$white = imagecolorallocate($jpg_image, $c1, $c2, $c3);
 
   // Set Path to Font File
-  $font_path = './arial.ttf';
+  //$font_path = './arial.ttf';
 
   // Set Text to Be Printed On Image
-  $text = $_POST['texto'];
+  //$text = $_POST['texto'];
 
   // Print Text On Image
-  imagettftext($jpg_image, 32, 0, 75, 75, $white, $font_path, $text);
-  imagettftext($jpg_image, 32, 0, 700, 500, $white, $font_path, 'www.AnimeXMega.net');
+  //imagettftext($jpg_image, 32, 0, 75, 75, $white, $font_path, $text);
+  //imagettftext($jpg_image, 32, 0, 700, 500, $white, $font_path, 'www.AnimeXMega.net');
 
   // Send Image to Browser
-  imagejpeg($jpg_image, 'imagen.jpg');
+  //imagejpeg($jpg_image, 'imagen.jpg');
 
   // Clear Memory
-  imagedestroy($jpg_image);
+  //imagedestroy($jpg_image);
 $file = '//www.youtubeinmp3.com/fetch/?video='.$_POST['youtube']; 
+echo $file;
 file_put_contents("mp3/audio.mp3", fopen($file, 'r'));
 ?>
