6a19aa41d19052e636db03f49f08894792147deb
Enzo Zapata <enzo0697@gmail.com>
2016-12-26 16:03:17 -0300

    checkeo de libreria gd

--- imagen.php
+++ imagen.php
@@ -23,3 +23,11 @@
   // Clear Memory
   imagedestroy($jpg_image);
 ?>
+<?php
+if (extension_loaded('gd') && function_exists('gd_info')) {
+    echo "PHP GD library is installed on your web server";
+}
+else {
+    echo "PHP GD library is NOT installed on your web server";
+}
+?>
