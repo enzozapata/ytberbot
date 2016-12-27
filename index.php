cd612ae207f1957ca93b498173d0fe173409e8f5
Enzo Zapata <enzo0697@gmail.com>
2016-12-26 21:24:22 -0300

    v0.9

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <form action="imagen.php" method="post" enctype="multipart/form-data">
 <label for="url_imagen">URL de la imagén:</label><input type="text" name="url_imagen"></br>
 <label for="texto">Texto:</label><input type="text" name="texto">
+<label for="texto">Descripcion YT:</label><input type="text" name="descripcion">
 <label for="youtube">Youtube:</label><input type="text" name="youtube">
 <input type="submit" value="Subir">
 </form>
